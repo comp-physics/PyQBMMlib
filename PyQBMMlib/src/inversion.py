b8e6053a9c7c43068cb5bece24971b7b686e4a0f
Spencer Bryngelson <sbryngelson@gmail.com>
2020-08-31 10:29:26 -0700

    more porting hyqmom3

--- PyQBMMlib/src/inversion.py
+++ PyQBMMlib/src/inversion.py
@@ -159,106 +159,112 @@ def hyperbolic(moms, max_skewness = 30):
     # needs to be ported to python
     # refer to HYQMOM3 in QBMMlib Mathematica
     if n=3:
-        etasmall = 10**(-10); 
-        verysmall = 10**(-14); 
-        realsmall = 10**(-14); 
-        n = Table[0, {i, 3}]; 
-        u = n; 
-        If[moms[1] <= verysmall, 
-            n[2] = moms[1]; 
-            Return[{n, u}, Module];
-        ]; 
-        bu = moms[2]/moms[1]; 
-        d2 = moms[3]/moms[1]; 
-        d3 = moms[4]/moms[1]; 
-        d4 = moms[5]/moms[1]; 
-        c2 = d2 - bu**2; 
-        c3 = d3 - 3*bu d2 + 2 bu**3; 
-        c4 = d4 - 4*bu d3 + 6 bu**2 d2 - 3 bu**4;
-        realizable = c2 c4 - c2**3 - c3**2; 
+        etasmall = 10**(-10) 
+        verysmall = 10**(-14) 
+        realsmall = 10**(-14) 
+
+        w = zeros(n)
+        x = w 
+        if moms[1] <= verysmall:
+            w[2] = moms[1] 
+            return w,x
+
+        bx = moms[2]/moms[1] 
+        d2 = moms[3]/moms[1] 
+        d3 = moms[4]/moms[1] 
+        d4 = moms[5]/moms[1] 
+        c2 = d2 - bx**2 
+        c3 = d3 - 3*bx*d2 + 2*bx**3 
+        c4 = d4 - 4*bx*d3 + 6*(bx**2)*d2 - 3*bx**4
+        realizable = c2*c4 - c2**3 - c3**2 
         if c2 < 0: 
             if c2 < -verysmall:
-                Print["Error: c2 negative in three node HYQMOM"];Abort[];]; 
+                print("Error: c2 negative in three node HYQMOM")
+                exit
             c2 = 0
             c3 = 0 
             c4 = 0
         else:
             if realizable < 0:
                 if c2 >= etasmall:
-                        q = c3/Sqrt[c2]/c2; 
-                        eta = c4/c2/c2; 
-                        if Abs[q] > verysmall :
-                            slope = (eta - 3)/q; 
-                            det = 8 + slope**2;
-                            qp = 0.5 (slope + Sqrt[det]); 
-                            qm = 0.5 (slope - Sqrt[det]); 
-                            if Sign[q] = 1: 
-                                q = qp
-                            else:
-                                q = qm
+                    q = c3/math.sqrt(c2)/c2 
+                    eta = c4/c2/c2 
+                    if abs(q) > verysmall:
+                        slope = (eta - 3)/q 
+                        det = 8 + slope**2
+                        qp = 0.5 (slope + math.sqrt(det)) 
+                        qm = 0.5 (slope - math.sqrt(det)) 
+                        if sign(q) = 1: 
+                            q = qp
                         else:
-                            q = 0;
-                        ]; 
-                        eta = q**2 + 1; 
-                        c3 = q Sqrt[c2] c2; c4 = eta c2**2; 
-                        if realizable < -10**(-6):
-                            Print["Error: c4 small in HYQMOM3"];
-                            Abort[];
-                    else: 
-                        c3 = 0; 
-                        c4 = c2**2;
-        scale = Sqrt[c2]; 
-        If[c2 >= etasmall, 
-            q = c3/Sqrt[c2]/c2; 
-            eta = c4/c2/c2;
-        , 
-            q = 0; 
-            eta = 1;
-        ]; 
-        If[
-            q**2 > qmax**2, 
-            slope = (eta - 3)/q; 
-            q = qmax Sign[q]; 
-            eta = 3 + slope q; 
-            realizable = eta - 1 - q**2; 
-            If[realizable < 0, eta = 1 + q**2];
-        ]; 
-        ups[1] = (q - Sqrt[4 eta - 3 q**2])/2; 
-        ups[2] = 0; 
-        ups[3] = (q + Sqrt[4 eta - 3 q**2])/2; 
-        dem = 1/Sqrt[4 eta - 3 q**2]; 
-        prod = -ups[1] ups[3]; 
-        prod = Max[prod, 1 + realsmall]; 
-        rho[1] = -dem/ups[1]; 
-        rho[2] = 1 - 1/prod; 
-        rho[3] = dem/ups[3]; 
-        srho = Sum[rho[i], {i, 3}]; 
-        Do[rho[i] = rho[i]/srho, {i, 3}]; 
-        scales = Sum[rho[i] ups[i]**2, {i, 3}]/Sum[rho[i], {i, 3}]; 
-        Do[up[i] = ups[i] scale/Sqrt[scales], {i, 3}]; 
-        If[
-            Min[Table[rho[i], {i, 3}] < 0, 
-            Print["Error: Negative weight in HYQMOM"];Abort[];
-        ]; 
-        n[1] = rho[1]; 
-        n[2] = rho[2]; 
-        n[3] = rho[3]; 
-        n = moms[1] n; 
+                            q = qm
+                    else:
+                        q = 0
 
-        u[1] = up[1]; 
-        u[2] = up[2]; 
-        u[3] = up[3]; 
-        u = bu + u; 
+                    eta = q**2 + 1 
+                    c3 = q*math.sqrt(c2)*c2 
+                    c4 = eta*c2**2 
+                    if realizable < -10.**(-6):
+                        print("Error: c4 small in HYQMOM3")
+                        exit
+                else: 
+                    c3 = 0.
+                    c4 = c2**2.
 
-        Return[{n, u}, Module];
-    ];
+        scale = math.sqrt(c2)
+        if c2 >= etasmall:
+            q = c3/math.sqrt(c2)/c2 
+            eta = c4/c2/c2
+        else: 
+            q = 0 
+            eta = 1
+
+        if q**2 > qmax**2:
+            slope = (eta - 3)/q 
+            q = qmax*sign(q)
+            eta = 3 + slope*q 
+            realizable = eta - 1 - q**2 
+            if realizable < 0:
+                eta = 1 + q**2
+
+        xps[1] = (q - math.sqrt(4*eta - 3*q**2))/2 
+        xps[2] = 0 
+        xps[3] = (q + math.sqrt(4*eta - 3*q**2))/2 
+
+        dem = 1/math.sqrt(4*eta - 3 q**2)
+        prod = -xps[1]*xps[3] 
+        prod = max(prod, 1 + realsmall)
+
+        rho[1] = -dem/xps[1] 
+        rho[2] = 1 - 1/prod 
+        rho[3] = dem/xps[3] 
+
+        srho = Sum[rho[i], {i, 3}] 
+        Do[rho[i] = rho[i]/srho, {i, 3}] 
+        scales = Sum[rho[i]*xps[i]**2, {i, 3}]/Sum[rho[i], {i, 3}] 
+        Do[xp[i] = xps[i]*scale/math.sqrt[scales], {i, 3}] 
+        if min[Table[rho[i], {i, 3}] < 0 :
+            print("Error: Negative weight in HYQMOM")
+            exit
+
+        w[1] = rho[1] 
+        w[2] = rho[2] 
+        w[3] = rho[3] 
+        w = moms[1]*w 
+
+        x[1] = xp[1] 
+        x[2] = xp[2] 
+        x[3] = xp[3] 
+        x = bx + x 
+
+        return w,x
 
 
     print('inversion: Warning: n is not 2 or 3 aborting')
 
-    weights   = np.array([])
-    abscissas = np.array([])    
-    return weights, abscissas
+    # weights   = np.array([])
+    # abscissas = np.array([])    
+    # return weights, abscissas
 
 ###
 ### Conditional inversion methods for 2+D problems
