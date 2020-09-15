6efa64b94ae9988127694d489c73aac2211edd96
Esteban Cisneros Garibay <ecisneros8@bitbucket.org>
2020-09-14 22:21:45 -0700

    Fleshing out advancer

--- PyQBMMlib/src/advancer.py
+++ PyQBMMlib/src/advancer.py
@@ -8,8 +8,24 @@ class advancer:
         self.time_step = config['advancer']['time_step']
         self.num_steps = config['advancer']['num_steps']
 
-        return
+        self.qbmm_mgr = qbmm_manager( config )
+
+        self.num_dim  = qbmm_mgr.num_moments
+        self.state    = np.zeros( self.num_dim )
+        self.rhs      = np.zeros( self.num_dim )
         
+        return
+
+    def initialize_moments(self):
+
+        return
+    
     def advance(self):
+
+        # self.rhs += self.qbmm_mgr.evaluate_rhs( self.state )
+        
+        return
+
+    def run(self):
 
         return
