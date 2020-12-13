b5af62b27abbece5f1e2dc294130b8eaee4f5b5f
Spencer Bryngelson <sbryngelson@gmail.com>
2020-12-12 22:26:05 -0800

    add projection to unit tests

--- test/test_qbmm.py
+++ test/test_qbmm.py
@@ -6,6 +6,49 @@ from stats_util import *
 import numpy.polynomial.hermite as hermite_poly
 import pytest
 
+def test_project():
+    tol = 1.e-10
+    success = True
+    
+    # 1D
+    config = {}
+    config['qbmm'] = {}
+    config['qbmm']['governing_dynamics']   = '4*x - 2*x**2'
+    config['qbmm']['num_internal_coords']  = 1
+    config['qbmm']['num_quadrature_nodes'] = 3
+    config['qbmm']['method']       = 'qmom'
+    qbmm_mgr = qbmm_manager( config )
+
+    mu  = 0.0
+    sig = 1.1
+    init_moments = raw_gaussian_moments_univar( qbmm_mgr.num_moments, mu, sig )
+    abscissas, weights = qbmm_mgr.moment_invert( init_moments )
+    projected_moments = qbmm_mgr.projection( weights, abscissas, qbmm_mgr.indices )
+    err_1D = np.linalg.norm(projected_moments - init_moments)
+
+    # 2D
+    config = {}
+    config['qbmm'] = {}
+    config['qbmm']['governing_dynamics']   = ' - xdot - x '
+    config['qbmm']['num_internal_coords']  = 2
+    config['qbmm']['num_quadrature_nodes'] = 4
+    config['qbmm']['method']       = 'chyqmom'
+    qbmm_mgr = qbmm_manager( config )
+    
+    mu  = [1.1,0.1]
+    sig = [1.0,1.0]
+    init_moments = raw_gaussian_moments_bivar( 
+            qbmm_mgr.indices,
+            mu[0], mu[1], 
+            sig[0], sig[1] 
+        )
+    abscissas, weights = qbmm_mgr.moment_invert( init_moments, qbmm_mgr.indices )
+    projected_moments = qbmm_mgr.projection( weights, abscissas, qbmm_mgr.indices )
+    err_2D = np.linalg.norm(projected_moments - init_moments)
+
+    assert err_1D < tol
+    assert err_2D < tol
+
 def test_wheeler():
     """
     This function tests QBMM Wheeler inversion by comparing
